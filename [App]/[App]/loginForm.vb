d0b28fca15bb234a70d421cdb6ea5b0d5106a5e1
vlad101vlad <vlad101vlad@gmail.com>
2018-11-03 16:20:05 +0200

    Included connection to the database

--- [App]/[App]/loginForm.vb
+++ [App]/[App]/loginForm.vb
@@ -1,5 +1,18 @@
-﻿Public Class loginForm
+﻿Imports MySql.Data.MySqlClient
+
+Public Class loginForm
+    Dim connection As New MySqlConnection
+
+    Private Sub Connect()
+        connection.ConnectionString = "server=localhost; userid=vladrares10; password=romania@_10; database=devbase;"
+        connection.Open()
+        MsgBox("You are connected to the database !")
+    End Sub
+
+
+
     Private Sub loginBTN_Click(sender As Object, e As EventArgs) Handles loginBTN.Click
         menuForm.Show()
+        Connect()
     End Sub
 End Class
